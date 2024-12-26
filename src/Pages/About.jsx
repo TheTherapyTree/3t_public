a6dc149822a43510238151e995470b19ea30b863
TheTherapyTree <contact.thetherapytree@gmail.com>
2024-12-26 21:28:27 +0530

    Included Helmet for SEO

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,42 +1,52 @@
 import React from "react";
 import PhoneIcon from '@mui/icons-material/Phone';
 import "../Styles/About.css";
+import { Helmet } from "react-helmet-async";
 
 const About = () => {
     return(
-        <div className="abtContentContainer">
-            <div className="abtTitle">
-                <span style={{fontSize : '2.75vh'}}>Our Story</span>
+        <>
+            <Helmet>
+                <title>About Us | The Therapy Tree</title>
+                <meta
+                    name="description"
+                    content="With the vision of providing affordable therapy for children with special needs, our story and mission is something to know about." />
+                <link rel="canonical" href="/about" />
+            </Helmet>
+            <div className="abtContentContainer">
+                <div className="abtTitle">
+                    <span style={{fontSize : '2.75vh'}}>Our Story</span>
+                </div>
+                <div className="abtSubTitle">
+                    <span style={{fontSize : '2.3vh'}}>Welcome to The Therapy Tree – A Beacon of Hope in Aganampudi</span>
+                </div>
+                <div>
+                    <p>Nestled in the heart of Aganampudi, Visakhapatnam, The Therapy Tree is a newly established therapy center dedicated to nurturing and supporting children with diverse therapeutic needs. Founded with a deep commitment to making a positive impact in the lives of children and their families, The Therapy Tree stands as a symbol of compassion, hope, and specialized care.</p>
+                </div>
+                <div>
+                    <p>Our journey began with a simple yet profound vision: to create a safe and supportive environment where children can receive the therapy they need to thrive. At The Therapy Tree, we believe that every child is unique and deserves individualized attention to help them reach their full potential.</p>
+                </div>
+                <div>
+                    <p>Our team of skilled and compassionate therapists works tirelessly to provide a range of services tailored to the specific needs of each child. Whether it's speech therapy, occupational therapy, physical therapy, or behavioral therapy, we are here to offer the best care possible. We utilize evidence-based practices and innovative techniques to ensure the highest quality of treatment.</p>
+                </div>
+                <div>
+                    <p>The Therapy Tree is more than just a therapy center; it's a community. We work closely with parents and caregivers, providing them with the support and resources they need to be active participants in their child's therapeutic journey. Our holistic approach ensures that the progress made in therapy extends beyond our center, fostering lasting positive changes in the children's lives.</p>
+                </div>
+                <div>
+                    <p>In a short time, The Therapy Tree has become a trusted name in Aganampudi, known for our dedication to excellence and our heartfelt commitment to each child's well-being. We invite you to join us on this journey of growth, hope, and transformation. Together, we can create a brighter future for our children.</p>
+                </div>
+                <div>
+                    <p>Visit us at The Therapy Tree and experience the difference that personalized, compassionate care can make.</p>
+                </div>
+                <button className="abtBtn">
+                    <a className="call" href="tel:919705315452">
+                            <PhoneIcon style={{fontSize : '2vh', marginRight:'min(2vw,8px)'}}/>
+                            <span>Contact Us</span>
+                        </a>
+                </button>
+                
             </div>
-            <div className="abtSubTitle">
-                <span style={{fontSize : '2.3vh'}}>Welcome to The Therapy Tree – A Beacon of Hope in Aganampudi</span>
-            </div>
-            <div>
-                <p>Nestled in the heart of Aganampudi, Visakhapatnam, The Therapy Tree is a newly established therapy center dedicated to nurturing and supporting children with diverse therapeutic needs. Founded with a deep commitment to making a positive impact in the lives of children and their families, The Therapy Tree stands as a symbol of compassion, hope, and specialized care.</p>
-            </div>
-            <div>
-                <p>Our journey began with a simple yet profound vision: to create a safe and supportive environment where children can receive the therapy they need to thrive. At The Therapy Tree, we believe that every child is unique and deserves individualized attention to help them reach their full potential.</p>
-            </div>
-            <div>
-                <p>Our team of skilled and compassionate therapists works tirelessly to provide a range of services tailored to the specific needs of each child. Whether it's speech therapy, occupational therapy, physical therapy, or behavioral therapy, we are here to offer the best care possible. We utilize evidence-based practices and innovative techniques to ensure the highest quality of treatment.</p>
-            </div>
-            <div>
-                <p>The Therapy Tree is more than just a therapy center; it's a community. We work closely with parents and caregivers, providing them with the support and resources they need to be active participants in their child's therapeutic journey. Our holistic approach ensures that the progress made in therapy extends beyond our center, fostering lasting positive changes in the children's lives.</p>
-            </div>
-            <div>
-                <p>In a short time, The Therapy Tree has become a trusted name in Aganampudi, known for our dedication to excellence and our heartfelt commitment to each child's well-being. We invite you to join us on this journey of growth, hope, and transformation. Together, we can create a brighter future for our children.</p>
-            </div>
-            <div>
-                <p>Visit us at The Therapy Tree and experience the difference that personalized, compassionate care can make.</p>
-            </div>
-            <button className="abtBtn">
-                <a className="call" href="tel:919705315452">
-                        <PhoneIcon style={{fontSize : '2vh', marginRight:'min(2vw,8px)'}}/>
-                        <span>Contact Us</span>
-                    </a>
-            </button>
-            
-        </div>
+        </>
     )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,6 +12,7 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import emailjs from '@emailjs/browser';
 
 import "../Styles/Home.css";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
 
@@ -60,153 +61,162 @@ const Home = () => {
       };
 
     return(
-        <div className="homeContainer">
-            <div className="greetSec">
-                <div className="greet">
-                    <span>You deserve a healing from the roots</span>
-                </div>
-            </div>
-            <div className="offerings">
-                <div className="offering">
-                    <FitnessCenterIcon style={{fontSize : '3vmax', color:'grey'}}/>
-                    
-                        <span>Occupational Therapy</span>
-                        <span style={{fontSize : '14px', color:'grey'}}>Enhancing daily function and independence.</span>
-                    
-                </div>
-                <div className="offering">
-                    <PsychologyIcon style={{fontSize : '3vmax', color:'grey'}}/>
-                    
-                        <span>Behaviour Therapy</span>
-                        <span style={{fontSize : '14px', color:'grey'}}>Change through reinforcement techniques.</span>
-                    
-                </div>
-                <div className="offering">
-                    <SpaIcon style={{fontSize : '3vmax', color:'grey'}}/>
-                    
-                        <span>Physiotherapy</span>
-                        <span style={{fontSize : '14px', color:'grey'}}>Restoring movement and relieving pain.</span>
-                    
-                </div>
-                <div className="offering">
-                    <RecordVoiceOverIcon style={{fontSize : '3vmax', color:'grey'}}/>
-                    
-                        <span>Speech Training</span>
-                        <span style={{fontSize : '14px', color:'grey'}}>Improving communication and speech abilities.</span>
-                    
-                </div>
-                <div className="offering">
-                    <MenuBookIcon style={{fontSize : '3vmax', color:'grey'}}/>
-                    
-                        <span>Special Education</span>
-                        <span style={{fontSize : '14px', color:'grey'}}>Supporting diverse learning and development.</span>
-                    
-                </div>
-            </div>
-            <div className="actionSec">
-                <div className="actionMsg" style={{fontSize : '2.3vh'}}>
-                    Get your free consultation TODAY!
-                </div>
-                <button>
-                    <a className="call" href="tel:919705315452">
-                        <PhoneIcon style={{fontSize : '2vh'}}/>
-                        <span>Call Now</span>
-                    </a>
-                </button>
-            </div>
-            <div className="expSec">
-                <div className="dataVal" >
-                    <span style={{fontSize : '2.25vh'}}>Therapy with</span>
-                    <span style={{fontSize : '3vh'}}> Indumathi</span>
-                </div>
-                <div className="dataSec">
-                    <div className="dataOpt">
-                        <div className="dataVal">
-                            <span className="dataVal1" >10</span>
-                            <span className="dataVal2" >+</span>
-                        </div>
-                        <span className="dataDesc">Years of Experience</span>
-                    </div>
-                    <div className="dataOpt">
-                        <div className="dataVal">
-                            <span className="dataVal1" >100</span>
-                            <span className="dataVal2" >+</span>
-                        </div>
-                        <span className="dataDesc">Children Interacted</span>
-                    </div>
-                    <div className="dataOpt">
-                        <div className="dataVal">
-                            <span className="dataVal1" >4.5</span>
-                            <span className="dataVal3"> / 5</span>
-                        </div>
-                        <span className="dataDesc">Positive Outlook</span>
+        <>
+            <Helmet>
+                <title>The Therapy Tree</title>
+                <meta 
+                    name="description" 
+                    content="Therapy Center in Aganampudi for children with special needs"/>
+                <link rel="canonical" href="/" />
+            </Helmet>
+            <div className="homeContainer">
+                <div className="greetSec">
+                    <div className="greet">
+                        <span>You deserve a healing from the roots</span>
                     </div>
                 </div>
-            </div>
-            <div className="contactSec">
-                <div className="locSec">
-                    <div className="locSecRow">
-                        <div className="locSecCol">
-                            <LocationOnIcon style={{fontSize: 'min(5vh, 50px)', color: 'tomato'}}/>
-                        </div>
-                        <div className="locSecCol">
-                            <span className="locTitle">Location</span>
-                            <span>Flat No 301</span>
-                            <span>Rajyalakshmi Ananda Vaibhavam</span>
-                            <span>Towards Duvvada Railway Station Road</span>
-                            <span>Aganampudi</span>
-                        </div>
+                <div className="offerings">
+                    <div className="offering">
+                        <FitnessCenterIcon style={{fontSize : '3vmax', color:'grey'}}/>
+                        
+                            <span>Occupational Therapy</span>
+                            <span style={{fontSize : '14px', color:'grey'}}>Enhancing daily function and independence.</span>
+                        
                     </div>
-                    <div className="locSecRow">
-                        <div className="locSecCol">
-                            <AlarmOnIcon style={{fontSize: 'min(5vh, 50px)', color: 'green'}}/>
+                    <div className="offering">
+                        <PsychologyIcon style={{fontSize : '3vmax', color:'grey'}}/>
+                        
+                            <span>Behaviour Therapy</span>
+                            <span style={{fontSize : '14px', color:'grey'}}>Change through reinforcement techniques.</span>
+                        
+                    </div>
+                    <div className="offering">
+                        <SpaIcon style={{fontSize : '3vmax', color:'grey'}}/>
+                        
+                            <span>Physiotherapy</span>
+                            <span style={{fontSize : '14px', color:'grey'}}>Restoring movement and relieving pain.</span>
+                        
+                    </div>
+                    <div className="offering">
+                        <RecordVoiceOverIcon style={{fontSize : '3vmax', color:'grey'}}/>
+                        
+                            <span>Speech Training</span>
+                            <span style={{fontSize : '14px', color:'grey'}}>Improving communication and speech abilities.</span>
+                        
+                    </div>
+                    <div className="offering">
+                        <MenuBookIcon style={{fontSize : '3vmax', color:'grey'}}/>
+                        
+                            <span>Special Education</span>
+                            <span style={{fontSize : '14px', color:'grey'}}>Supporting diverse learning and development.</span>
+                        
+                    </div>
+                </div>
+                <div className="actionSec">
+                    <div className="actionMsg" style={{fontSize : '2.3vh'}}>
+                        Get your free consultation TODAY!
+                    </div>
+                    <button>
+                        <a className="call" href="tel:919705315452">
+                            <PhoneIcon style={{fontSize : '2vh'}}/>
+                            <span>Call Now</span>
+                        </a>
+                    </button>
+                </div>
+                <div className="expSec">
+                    <div className="dataVal" >
+                        <span style={{fontSize : '2.25vh'}}>Therapy with</span>
+                        <span style={{fontSize : '3vh'}}> Indumathi</span>
+                    </div>
+                    <div className="dataSec">
+                        <div className="dataOpt">
+                            <div className="dataVal">
+                                <span className="dataVal1" >10</span>
+                                <span className="dataVal2" >+</span>
+                            </div>
+                            <span className="dataDesc">Years of Experience</span>
                         </div>
-                        <div className="locSecCol">
-                            <span className="locTitle">Opening Hours</span>
-                            <span>Monday - Saturday</span>
-                            <span>9:00 am - 6:00 pm</span>
+                        <div className="dataOpt">
+                            <div className="dataVal">
+                                <span className="dataVal1" >100</span>
+                                <span className="dataVal2" >+</span>
+                            </div>
+                            <span className="dataDesc">Children Interacted</span>
+                        </div>
+                        <div className="dataOpt">
+                            <div className="dataVal">
+                                <span className="dataVal1" >4.5</span>
+                                <span className="dataVal3"> / 5</span>
+                            </div>
+                            <span className="dataDesc">Positive Outlook</span>
                         </div>
                     </div>
                 </div>
-                <div className="formSec">
-                    <form ref={form} className="fsForm" >
-                        <div className="formRow">
-                            <input name="firstName" value={formData.firstName} placeholder="FirstName" type="text" onChange={handleChange} />
-                            <input name="lastName" value={formData.lastName} placeholder="LastName" type="text" onChange={handleChange} />
-                        </div>
-                        <div className="formRow">
-                            <input name="dob" value={formData.dob} placeholder="Date of Birth" type="text" onFocus={(e) => {e.target.type = "date"; e.target.showPicker()}} onBlur={(e) => e.target.type = "text"} style={{width : 'max(10vw, 100px)'}} onChange={handleChange}/>
-                            <input name="age" value={formData.age} placeholder="age" type="number" style={{width : 'max(4vw, 28px)'}} onChange={handleChange}/>
-                            {/* <input placeholder="services" type="text" /> */}
-                            <select name="service" value={formData.service} className="serv"  onChange={handleChange}>
-                                <option value={""}>Choose a service</option>
-                                <option value={"GA"}>General Assessment</option>
-                                <option value={"OT"}>Occupational Therapy</option>
-                                <option value={"BT"}>Behaviour Therapy</option>
-                                <option value={"PT"}>Physiotherapy</option>
-                                <option value={"ST"}>Speech Therapy</option>
-                                <option value={"SE"}>Special Education</option>
-                            </select>
-                        </div>
-                        <div className="formRow"> 
-                            <input name="parentName" value={formData.parentName} placeholder="Parent's Name" type="text" onChange={handleChange} />
-                            <input name="contactNo" value={formData.contactNo} placeholder="Contact Number" type="number" onChange={handleChange} />
-                        </div>
-                        <div className="formRow"> 
-                            <input name="msg" value={formData.msg} placeholder="Message" type="text" style={{height : '12vh', width : 'max(20vw, 150px)'}} onChange={handleChange}/>
-                            <div className="formCol">
-                                <input name="apptDate" value={formData.apptDate} placeholder="Date of Appointment" type="text" onFocus={(e) => {e.target.type = "date"; e.target.showPicker()}} onBlur={(e) => e.target.type = "text"}  onChange={handleChange}/>
-                                <input name="apptTime" value={formData.apptTime} placeholder="slot" type="text" onFocus={(e) => {e.target.type = "time"; e.target.showPicker()}} onBlur={(e) => e.target.type = "text"}  onChange={handleChange}/>
+                <div className="contactSec">
+                    <div className="locSec">
+                        <div className="locSecRow">
+                            <div className="locSecCol">
+                                <LocationOnIcon style={{fontSize: 'min(5vh, 50px)', color: 'tomato'}}/>
+                            </div>
+                            <div className="locSecCol">
+                                <span className="locTitle">Location</span>
+                                <span>Flat No 301</span>
+                                <span>Rajyalakshmi Ananda Vaibhavam</span>
+                                <span>Towards Duvvada Railway Station Road</span>
+                                <span>Aganampudi</span>
                             </div>
                         </div>
-                        <button onClick={sendEmail}>
-                            <CalendarMonthIcon style={{fontSize : '2.5vh'}}/>
-                            Book Appointment
-                        </button>
-                    </form>
+                        <div className="locSecRow">
+                            <div className="locSecCol">
+                                <AlarmOnIcon style={{fontSize: 'min(5vh, 50px)', color: 'green'}}/>
+                            </div>
+                            <div className="locSecCol">
+                                <span className="locTitle">Opening Hours</span>
+                                <span>Monday - Saturday</span>
+                                <span>9:00 am - 6:00 pm</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="formSec">
+                        <form ref={form} className="fsForm" >
+                            <div className="formRow">
+                                <input name="firstName" value={formData.firstName} placeholder="FirstName" type="text" onChange={handleChange} />
+                                <input name="lastName" value={formData.lastName} placeholder="LastName" type="text" onChange={handleChange} />
+                            </div>
+                            <div className="formRow">
+                                <input name="dob" value={formData.dob} placeholder="Date of Birth" type="text" onFocus={(e) => {e.target.type = "date"; e.target.showPicker()}} onBlur={(e) => e.target.type = "text"} style={{width : 'max(10vw, 100px)'}} onChange={handleChange}/>
+                                <input name="age" value={formData.age} placeholder="age" type="number" style={{width : 'max(4vw, 28px)'}} onChange={handleChange}/>
+                                {/* <input placeholder="services" type="text" /> */}
+                                <select name="service" value={formData.service} className="serv"  onChange={handleChange}>
+                                    <option value={""}>Choose a service</option>
+                                    <option value={"GA"}>General Assessment</option>
+                                    <option value={"OT"}>Occupational Therapy</option>
+                                    <option value={"BT"}>Behaviour Therapy</option>
+                                    <option value={"PT"}>Physiotherapy</option>
+                                    <option value={"ST"}>Speech Therapy</option>
+                                    <option value={"SE"}>Special Education</option>
+                                </select>
+                            </div>
+                            <div className="formRow"> 
+                                <input name="parentName" value={formData.parentName} placeholder="Parent's Name" type="text" onChange={handleChange} />
+                                <input name="contactNo" value={formData.contactNo} placeholder="Contact Number" type="number" onChange={handleChange} />
+                            </div>
+                            <div className="formRow"> 
+                                <input name="msg" value={formData.msg} placeholder="Message" type="text" style={{height : '12vh', width : 'max(20vw, 150px)'}} onChange={handleChange}/>
+                                <div className="formCol">
+                                    <input name="apptDate" value={formData.apptDate} placeholder="Date of Appointment" type="text" onFocus={(e) => {e.target.type = "date"; e.target.showPicker()}} onBlur={(e) => e.target.type = "text"}  onChange={handleChange}/>
+                                    <input name="apptTime" value={formData.apptTime} placeholder="slot" type="text" onFocus={(e) => {e.target.type = "time"; e.target.showPicker()}} onBlur={(e) => e.target.type = "text"}  onChange={handleChange}/>
+                                </div>
+                            </div>
+                            <button onClick={sendEmail}>
+                                <CalendarMonthIcon style={{fontSize : '2.5vh'}}/>
+                                Book Appointment
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
